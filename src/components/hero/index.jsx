006fcac3854984e30075a66e25753f2fdda0e020
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-25 13:18:13 +0700

    change margin description on hero section

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -32,7 +32,7 @@ export default function Hero() {
       <dh-component>
         <div className='bg-transparent'>
           <div className='container mx-auto flex flex-col items-center py-12 sm:py-24'>
-            <div className='w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10'>
+            <div className='w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-4'>
               <h1 className='text-1xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-extrabold leading- md:leading-10 '>
                 The Freedom to Create the{' '}
                 <span className='link link-underline link-underline-black text-indigo-700'>
@@ -40,7 +40,7 @@ export default function Hero() {
                 </span>{' '}
                 You Want
               </h1>
-              <p className='antialiased mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg'>
+              <p className='antialiased mt-5 sm:mt-5 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg'>
                 A professonal website drives sales. Create a beautiful website
                 to impress and engage new customers and establish your business
                 online{' '}
