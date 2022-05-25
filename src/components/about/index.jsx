313bf2aa4247659a9d4bd6306a8979d7ba3c15c4
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-25 13:54:36 +0700

    @refactor/add responsive about section and add custom hooks for resizing lottie assets

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useMemo } from 'react';
 import Lottie from 'react-lottie';
 
 import AnimationAbout from '../../assets/lotties/team-work.json';
+import { useSizeComponents } from '../../utils/util';
 
 export default function About() {
+  const [width, height] = useSizeComponents();
+  const scaleLottie = 0.5;
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -12,27 +17,33 @@ export default function About() {
       preserveAspectRatio: 'xMidYMid slice',
     },
   };
+
+  const animation = useMemo(() => {
+    if (!width) return null;
+    const xMidYMid = 1;
+    const sizeComponent = {
+      width: width * scaleLottie,
+      height: width * scaleLottie * xMidYMid,
+    };
+    return <Lottie key={width} options={defaultOptions} {...sizeComponent} />;
+  }, [width]);
+
   return (
     <div className='pb-12 mx-28' id='about'>
       <div className='grid grid-cols-2 md:grid-cols-2 bg-gray-100 rounded py-10 px-11'>
-        <div className='pt-8'>
-          <p className='leading-tight text-5xl font-semibold md:text-3xl text-gray-800 mb-4'>
+        <div className='flex pt-8 sm:items-center'>
+          <p className='leading-tight text-5xl md:text-3xl sm:text-2xl sm:pr-2 font-semibold text-gray-800 mb-4'>
             Having a hard time to build an modern website?
           </p>
         </div>
         <div className='pt-8'>
-          <p className='leading-tight text-3xl md:text-2xl text-gray-400 mb-4'>
-            Excepteur est anim quis ut. Consectetur Lorem sunt eu et consectetur
-            sint aliquip est non veniam minim est excepteur. Aute cupidatat esse
-            Lorem cupidatat. Consectetur velit non aute laborum fugiat deserunt
-            proident ullamco ex adipisicing id. Duis in enim enim veniam irure
-            labore laborum pariatur pariatur. Aute reprehenderit id elit magna
-            amet pariatur mollit eiusmod duis dolore quis.
+          <p className='leading-tight text-3xl text-justify md:text-2xl sm:text-lg sm:text-left text-gray-400 mb-4'>
+            Want to build fast and powerfull website, but doesn't know how to
+            figure it out. We here to help you to make your dream website become
+            real.
           </p>
         </div>
-        <div className='col-span-2 pt-12'>
-          <Lottie options={defaultOptions} height={500} width={600} />
-        </div>
+        <div className='col-span-2 pt-10'>{animation}</div>
       </div>
     </div>
   );
---
+++ src/utils/util.jsx
@@ -0,0 +1,19 @@
+import { useLayoutEffect, useState } from 'react';
+
+export const useSizeComponents = (ref) => {
+  const [size, setSize] = useState([0, 0]);
+
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      let newSize = [window.innerWidth, window.innerHeight];
+      if (ref?.current) {
+        newSize = [ref.current.offsetWidth, ref.current.offsetHeight];
+      }
+      setSize(newSize);
+    };
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
